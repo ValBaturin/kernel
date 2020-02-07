d0f818fc0d7868969f2362b48e5d6ad374b51539
Val <valeryxbaturin@gmail.com>
2020-02-07 20:48:35 +0300

    nice init version of mfs. arg parse works nicely

---
+++ mockfs/mockfs.c
@@ -0,0 +1,38 @@
+#include <editline/readline.h>
+#include <argp.h>
+
+const char *argp_program_version = "0.1";
+const char *argp_program_bug_address = "<val.baturin@serokell.io> or <val@baturin.me>";
+static char doc[] = "mockfs";
+static char args_doc[] = "";
+static struct argp_option options[] = { 
+    { "persist", 'e', 0, 0, "persistant storage, so reliable (NOT IMPLEMENTED THO)"},
+    { 0 } 
+};
+
+struct arguments {
+    enum { PRST, NPRST } mode;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    struct arguments *arguments = state->input;
+    switch (key) {
+    case 'p': arguments->mode = PRST; break;
+    case ARGP_KEY_ARG: return 0;
+    default: return ARGP_ERR_UNKNOWN;
+    }   
+    return 0;
+}
+ 
+static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
+
+int main(int argc, char** argv) {
+
+    struct arguments arguments;
+
+    arguments.mode = NPRST;
+
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+    
+    return 0;
+}

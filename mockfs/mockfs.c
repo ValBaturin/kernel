86d3799795dc47e8d4780dddf60933d767a66c46
Val <valeryxbaturin@gmail.com>
2020-02-08 21:32:45 +0300

    parse fs structure

--- mockfs/mockfs.c
+++ mockfs/mockfs.c
@@ -1,47 +1,66 @@
 #include <stdlib.h>
+#include <string.h>
 
-#include <editline/readline.h>
 #include <argp.h>
+#include <editline/readline.h>
 
 const char *argp_program_version = "0.1";
-const char *argp_program_bug_address = "<val.baturin@serokell.io> or <val@baturin.me>";
+const char *argp_program_bug_address =
+    "<val.baturin@serokell.io> or <val@baturin.me>";
 static char doc[] = "mockfs";
 static char args_doc[] = "";
-static struct argp_option options[] = { 
-    { "persist", 'e', 0, 0, "persistant storage, so reliable (NOT IMPLEMENTED THO)"},
-    { 0 } 
-};
+static struct argp_option options[] = {
+    {"persist", 'e', 0, 0,
+     "persistant storage, so reliable (NOT IMPLEMENTED THO)"},
+    {0}};
 
 struct arguments {
-    enum { PRST, NPRST } mode;
+  enum { PRST, NPRST } mode;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
-    struct arguments *arguments = state->input;
-    switch (key) {
-    case 'p': arguments->mode = PRST; break;
-    case ARGP_KEY_ARG: return 0;
-    default: return ARGP_ERR_UNKNOWN;
-    }   
+  struct arguments *arguments = state->input;
+  switch (key) {
+  case 'p':
+    arguments->mode = PRST;
+    break;
+  case ARGP_KEY_ARG:
     return 0;
+  default:
+    return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
 }
- 
-static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 
-int main(int argc, char** argv) {
+static struct argp argp = {options, parse_opt, args_doc, doc, 0, 0, 0};
 
-    struct arguments arguments;
+int main(int argc, char **argv) {
 
-    arguments.mode = NPRST;
+  struct arguments arguments;
 
-    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+  arguments.mode = NPRST;
 
-    while(1) {
-        char* input = readline("> ");
+  argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
+  while (1) {
+    char *input = readline("> ");
 
-        add_history(input);
-        free(input);
+    char *token = strtok(input, " ");
+    if (strcmp(token, "cd") == 0) {
+      printf("cd\n");
+    } else if (strcmp(token, "pwd") == 0) {
+      printf("pwd\n");
     }
-    return 0;
+
+    token = strtok(NULL, "/");
+    printf("->%c\n", token[-1]);
+    while (token != NULL) {
+      printf("->%s\n", token);
+      token = strtok(NULL, "/");
+    }
+
+    add_history(input);
+    free(input);
+  }
+  return 0;
 }

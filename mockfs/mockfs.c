1223d86beddaf6740ac3ad31782bb40ea05b9fcc
Val <valeryxbaturin@gmail.com>
2020-02-09 18:31:45 +0300

    mkdir ls work

--- mockfs/mockfs.c
+++ mockfs/mockfs.c
@@ -66,12 +66,12 @@ void init_fs(struct filesystem *fs, int size) {
   fs->inodes = calloc(fs->capacity, sizeof(struct inode *));
   fs->current_ptr = 0;
 
+  fs->inodes[fs->current_ptr] = malloc(sizeof(struct inode *));
   struct inode *curr = fs->inodes[fs->current_ptr];
-  curr = malloc(sizeof(struct inode));
   curr->size = 0;
   curr->type = FSDIR;
   curr->name = "/";
-  curr->data = malloc(1); // init some memory for later use of realloc
+  curr->data = malloc(10); // init some memory for later use of realloc
 }
 
 int main(int argc, char **argv) {
@@ -88,28 +88,39 @@ int main(int argc, char **argv) {
 
   char *current_dir = "/";
   char prompt[256];
-  snprintf(prompt, sizeof(prompt), "%s\n>", current_dir);
+  snprintf(prompt, sizeof(prompt), "%s\n> ", current_dir);
 
   while (1) {
     char *input = readline(prompt);
     add_history(input);
 
     char *token = strtok(input, " ");
+
     if (strcmp(token, "cd") == 0) {
       printf("cd\n");
     } else if (strcmp(token, "mkdir") == 0) {
+      is_absolute(&token);
       curr->size++;
       curr->data = realloc(curr->data, (curr->size) * sizeof(struct inode *));
-      struct inode *new_dir = (curr->data[curr->size - 1]);
+      curr->data[curr->size - 1] = malloc(sizeof(struct inode));
+
+      struct inode *new_dir = curr->data[curr->size - 1];
       new_dir->size = 0;
       new_dir->type = FSDIR;
+
       new_dir->name = malloc(80 * sizeof(char *));
-      snprintf(new_dir->name, sizeof(new_dir->name), "%s\n>", token);
+
+      snprintf(new_dir->name, sizeof(new_dir->name), "%s", token);
       new_dir->data = malloc(1); // init some memory for later use of realloc
+
     } else if (strcmp(token, "touch") == 0) {
       printf("touch\n");
     } else if (strcmp(token, "ls") == 0) {
-      printf("ls\n");
+      struct inode **content = curr->data;
+      for (int i = 0; i < curr->size; i++) {
+        printf("%s\n", content[i]->name);
+      }
+
     } else if (strcmp(token, "rm") == 0) {
       printf("rm\n");
     } else if (strcmp(token, "rmdir") == 0) {
@@ -118,11 +129,10 @@ int main(int argc, char **argv) {
       printf("cat\n");
     }
 
-    printf("ABS: %i\n", is_absolute(&token));
-    while (token != NULL) {
-      printf("->%s\n", token);
-      next_obj(&token);
-    }
+    // while (token != NULL) {
+    //  printf("->%s\n", token);
+    //  next_obj(&token);
+    //}
 
     free(input);
   }
